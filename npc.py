80aa000818032fbf3db449d73e9118243f06dfca
WHITEBOYEATS <connetjosh777@gmail.com>
2024-09-24 12:39:47 -0500

    Bad_Version

---
+++ npc.py
@@ -0,0 +1,25 @@
+from sprite_object import *
+from random import randint, random, choice
+
+
+class NPC(AnimatedSprite):
+    def __init__(self, game, path='resources/sprites/npc/blob/0.png', pos=(10.5, 5.5), scale=0.6, shift=0.38, animation_time=180):
+        super().__init__(game, path, pos, scale, shift, animation_time)
+        self.attack_images = self.get_images(self.path + '/attack')
+        self.death_images = self.get_images(self.path + '/death')
+        self.idle_images = self.get_images(self.path + '/idle')
+        self.pain_images = self.get_images(self.path + '/pain')
+        self.walk_images = self.get_images(self.path + '/walk')
+        
+        self.attack_dist = randint(3,6)
+        self.speed = 0.03
+        self.size = 10
+        self.health = 100
+        self.attack_damage = 10
+        self.accuracy = 0.15
+        self.alive = True
+        self.pain = False
+
+    def update(self):
+        self.check_animation_time()
+        self.get_sprite()
--- object_handler.py
+++ object_handler.py
@@ -1,12 +1,16 @@
 from sprite_object import *
+from npc import *
 
 class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
+        self.npc_list = []
+        self.npc_sprite_path = 'resources/sprites/npc/'
         self.static_sprite_path = 'resources/sprites/static_sprites/'
         self.anim_sprite_path = 'resources/sprites/animated_sprites/'
         add_sprite = self.add_sprite
+        add_npc = self.add_npc
 
         add_sprite(SpriteObject(game))
         add_sprite(SpriteObject(game, path=self.static_sprite_path + 'comum.png', pos=(1.5, 3), scale=1, shift=0))
@@ -15,8 +19,16 @@ class ObjectHandler:
         add_sprite(AnimatedSprite(game, pos=(1.5, 1.5)))
         add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(20, 14.5)))
 
+
+        #npc
+        add_npc(NPC(game))
+
     def update(self):
         [sprite.update() for sprite in self.sprite_list]
+        [npc.update() for npc in self.npc_list]
+
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
 
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
